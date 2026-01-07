3b1530258d6fde8dd56cd7405dabb330a5e081fa
Roshni Giri <roshanigiri82@gmail.com>
2026-01-08 00:57:16 +0545

    profile section updated

--- deadStock/src/component/profile/buyerProfile.jsx
+++ deadStock/src/component/profile/buyerProfile.jsx
@@ -1,19 +1,31 @@
 import { Container } from "@mui/material";
 import ProfileHeader from "./profileHeader";
 import ProfileInfoCard from "./profileInfo";
-import { authUser } from "../data/authUser"; // your logged-in user
+import { authUser } from "../data/authUser"; 
+import SectionCard from "../common/sectionCard";
+import OrdersList from "./ordersList";
+import ShippingInfo from "./shippingInfo";
+
 
 const buyerProfile = () => {
   return (
     <Container
-      maxWidth="sm"
+      maxWidth="lg"
       sx={{ bgcolor: "#fff", minHeight: "100vh", py: 3 }}
     >
+      <SectionCard user={authUser}/>
       {/* Pass authUser to ProfileHeader */}
       <ProfileHeader user={authUser} />
 
       {/* Seller-specific info card */}
+
       <ProfileInfoCard user={authUser} />
+
+      <ShippingInfo user={authUser} />
+
+      <OrdersList user={authUser} />
+    
+
     </Container>
   );
 };
