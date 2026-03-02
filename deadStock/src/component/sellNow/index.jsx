61993d28f190937268861295eadf48d611b489f7
Sam-ima <samimamiya091@gmail.com>
2026-03-02 11:43:34 +0545

    placed slight bg color on sell not alternatively

--- deadStock/src/component/sellNow/index.jsx
+++ deadStock/src/component/sellNow/index.jsx
@@ -1,18 +1,31 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import HeroSection from "./firstHeroSection";
 import HowItWorks from "./howItWorks";
 import SellerTypes from "./sellerTypes";
 import SellingStyles from "./sellingStyles";
 
-
 const MainSellingPage = () => {
   return (
     <Box sx={{ mt: { xs: 8, sm: 10, md: 12 } }}>
-      <HeroSection />
-      <HowItWorks />
-      <SellerTypes />
-      <SellingStyles />
-      {/* <CTASection /> */}
+      <Container maxWidth="lg">
+        <HeroSection />
+      </Container>
+
+      <Box sx={{ bgcolor: "grey.50" }}>
+        <Container maxWidth="lg">
+          <HowItWorks />
+        </Container>
+      </Box>
+
+      <Container maxWidth="lg">
+        <SellerTypes />
+      </Container>
+
+      <Box sx={{ bgcolor: "grey.50" }}>
+        <Container maxWidth="lg">
+          <SellingStyles />
+        </Container>
+      </Box>
     </Box>
   );
 };
